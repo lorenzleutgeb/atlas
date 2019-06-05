81364b9c2b7ad84a0925de7f94d6b013f9bcfeb1
Lorenz Leutgeb <lorenz@leutgeb.xyz>
2019-06-05 10:50:05 +0200

    Fix postorder traversal

--- src/test/resources/traversals.ml
+++ src/test/resources/traversals.ml
@@ -1,15 +1,14 @@
-(* order :: ((Tree a) * (Tree a)) -> (Tree a) *)
+(* order :: (T * T) -> T *)
 
 inorder t q = match t with
-  | nil       ->                       q
+  | nil       -> q
   | (l, x, r) -> (inorder l (inorder r q, x, nil))
 
 preorder t q = match t with
-  | nil       ->                         q
+  | nil       -> q
   | (l, x, r) -> (preorder l (preorder r q), x, nil)
 
-
 postorder t q = match t with
-  | nil       ->                           q
-  | (l, x, r) -> (postorder r (postorder l q), x, nil)
+  | nil       -> q
+  | (l, x, r) -> (postorder l (postorder r (q, x, nil)))
 
