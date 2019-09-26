5a612b0f67cd6a94e25ac9441e871c13dec6c599
Lorenz Leutgeb <lorenz@leutgeb.xyz>
2019-09-26 18:08:24 +0200

    Add vutinfth artifacts
    Built from
    
        https://gitlab.com/ThomasAUZINGER/vutinfth.git
    
    at revision
    
        0cc133542d73765d39e671099ff7cc2ffac2c20a

---
+++ doc/build-thesis.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# Copyright (C) 2014-2019 by Thomas Auzinger <thomas@auzinger.name>
+
+# Replace the 'x' in the next line with the name of the thesis' main LaTeX document without the '.tex' extension
+SOURCE=example
+
+# Build the thesis document
+pdflatex $SOURCE
+bibtex   $SOURCE
+pdflatex $SOURCE
+pdflatex $SOURCE
+makeindex -t $SOURCE.glg -s $SOURCE.ist -o $SOURCE.gls $SOURCE.glo
+makeindex -t $SOURCE.alg -s $SOURCE.ist -o $SOURCE.acr $SOURCE.acn
+makeindex -t $SOURCE.ilg -o $SOURCE.ind $SOURCE.idx
+pdflatex $SOURCE
+pdflatex $SOURCE
+
+echo
+echo
+echo Thesis document compiled.
