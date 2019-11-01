b33990da7840667cc1f8d769c551930a6e65b0e4
Lorenz Leutgeb <lorenz@leutgeb.xyz>
2019-11-01 13:47:49 +0100

    int: s/logs/lac/ in Setup.hs

--- interpreter/Setup.hs
+++ interpreter/Setup.hs
@@ -6,8 +6,8 @@ main = do
   putStrLn "Compiling sources to generated-src. You'll need Java, Gradle, and unzip set up for this to work."
   result <- readProcess "../typechecker/gradlew" ["-p", "../typechecker", "distZip"] ""
   putStrLn result
-  result <- readProcess "unzip" ["-o", "../typechecker/build/distributions/logs.zip"] ""
+  result <- readProcess "unzip" ["-o", "../typechecker/build/distributions/lac.zip"] ""
   putStrLn result
-  result <- readProcess "../typechecker/build/distributions/logs/bin/logs" ["hs", "--home=../typechecker/src/test/resources", "\".*\"", "generated-src"] ""
+  result <- readProcess "../typechecker/build/distributions/lac/bin/lac" ["hs", "--home=../typechecker/src/test/resources", "\".*\"", "generated-src"] ""
   putStrLn result
   defaultMain
