101f2a08b4ec163f9bee29d4a86563c46f195dda
Lorenz Leutgeb <lorenz@leutgeb.xyz>
2019-11-15 14:44:37 +0100

    tcr: Add two more nontermination examples

--- typechecker/src/test/resources/Infinite.ml
+++ typechecker/src/test/resources/Infinite.ml
@@ -14,6 +14,10 @@ infinite_6 x t1 t2 = ((infinite_2 x t1), x, t2)
 
 infinite_7 x t1 t2 = (let s = t1 in ((infinite_2 x s), x, t2))
 
+infinite_8 x t = (let s = t in (infinite_8 x s, x, s))
+
+infinite_9 x t1 t2 = (let s = t2 in (infinite_9 x s t1, x, t1))
+
 (**
  * The following is an interesting case, but the current implementation cannot type mutual recursion.
  *)
