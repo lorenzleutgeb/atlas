fae669532d2f0fd00a3c748bb3f658bdfbd4883e
Lorenz Leutgeb <lorenz@leutgeb.xyz>
2020-09-25 09:10:58 +0200

    int: output more details in Setup.hs

--- interpreter/Setup.hs
+++ interpreter/Setup.hs
@@ -1,13 +1,17 @@
 import Distribution.Simple
+import System.Environment
 import System.Process
 
 main :: IO ()
 main = do
+  result <- getEnvironment
+  putStrLn (unlines (map (\(a, b) -> a ++ "=" ++ b) result))
   putStrLn "Compiling sources to generated-src. You'll need Java, Gradle, and unzip set up for this to work."
-  result <- readProcess "../typechecker/gradlew" ["-p", "../typechecker", "distZip"] ""
-  putStrLn result
-  result <- readProcess "unzip" ["-o", "../typechecker/build/distributions/lac.zip"] ""
-  putStrLn result
-  result <- readProcess "./lac/bin/lac" ["hs", "--home=../typechecker/src/test/resources", "\".*\"", "generated-src"] ""
-  putStrLn result
+  result <- readProcessWithExitCode "../typechecker/gradlew" ["-p", "../typechecker", "distZip"] ""
+  putStrLn (show result)
+  result <- readProcessWithExitCode "unzip" ["-o", "../typechecker/build/distributions/lac.zip"] ""
+  putStrLn (show result)
+  result <- readProcessWithExitCode "./lac/bin/lac" ["hs", "--home=../typechecker/src/test/resources", "\".*\"", "generated-src"] ""
+  putStrLn (show result)
+  defaultMain
   defaultMain
